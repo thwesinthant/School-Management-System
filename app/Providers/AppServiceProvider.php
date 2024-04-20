ad0c42e8af674fb0525eccc894ffd71b160bd769
thwesinthant <thwesinthant4@gmail.com>
2024-04-20 14:03:10 +0630

    13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-
-
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();
     }
 }
--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -8,7 +8,7 @@
              <div class="container-fluid">
                  <div class="row mb-2">
                      <div class="col-sm-6">
-                         <h1>Admin List</h1>
+                         <h1>Admin List (Total : {{ $getRecord->total() }} )</h1>
                      </div>
                      <div class="col-sm-6" style="text-align: right">
                          <a class="btn btn-primary" href="{{ url('admin/admin/add') }}">Add Admin</a>
@@ -16,6 +16,45 @@
                  </div>
              </div><!-- /.container-fluid -->
          </section>
+
+         {{-- Search Admin Form --}}
+         <div class="row" style="margin:7.5px ">
+             <div class="col-md-12">
+                 <div class="card">
+                     <div class="card-header">
+                         <h3 class="card-title">Search Admin</h3>
+                     </div>
+                     <!-- form start -->
+                     <form action="" method="get">
+                         <div class="card-body">
+                             <div class="row">
+                                 <div class="form-group col-md-3">
+                                     <label>Name</label>
+                                     <input type="text" name="name" value="{{ Request::get('name') }}"
+                                         class="form-control" placeholder="Enter name">
+                                 </div>
+                                 <div class="form-group col-md-3" style="margin-bottom:0px;">
+                                     <label>Email address</label>
+                                     <input type="text" name="email" value="{{ Request::get('email') }}"
+                                         class="form-control" placeholder="Enter email">
+                                 </div>
+                                 <div class="form-group col-md-3" style="margin-bottom:0px;">
+                                     <label>Date</label>
+                                     <input type="date" name="date" value="{{ Request::get('date') }}"
+                                         class="form-control">
+                                 </div>
+                                 <div class="form-group col-md-3" style="margin-top:30px;">
+                                     <button type="submit" class="btn btn-primary">Search</button>
+                                     <a href="{{ url('admin/admin/list') }}" class="btn btn-success">Reset</a>
+                                 </div>
+                             </div>
+                         </div>
+
+
+                     </form>
+                 </div>
+             </div>
+         </div>
 
          <!-- Main content -->
          <section class="content">
@@ -46,7 +85,8 @@
                                                  <td>{{ $value->id }}</td>
                                                  <td>{{ $value->name }}</td>
                                                  <td>{{ $value->email }}</td>
-                                                 <td>{{ $value->created_at }}</td>
+                                                 <td>{{ date('m-d-Y   H:i A'), strtotime($value->created_at) }}
+                                                 </td>
                                                  <td style="text-align: center; ">
                                                      <a href="{{ url('admin/admin/edit', $value->id) }}"
                                                          class="btn btn-primary">Edit</a>
@@ -57,6 +97,10 @@
                                          @endforeach
                                      </tbody>
                                  </table>
+                                 <div style="float: right;padding:10px;">
+                                     {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}</div>
+
+
                              </div>
                              <!-- /.card-body -->
                          </div>
