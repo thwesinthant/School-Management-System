5ce4fe00c50001600188a7947238171a8990d52e
thwesinthant <thwesinthant4@gmail.com>
2024-05-05 21:44:58 +0630

    Part 37 completed

--- resources/views/admin/assign_class_teacher/list.blade.php
+++ resources/views/admin/assign_class_teacher/list.blade.php
@@ -8,16 +8,66 @@
              <div class="container-fluid">
                  <div class="row mb-2">
                      <div class="col-sm-6">
-                         <h1>Assign Class Teacher</h1>
+                         <h1>Assign Class Teacher (Total : {{ $getRecord->total() }})</h1>
                      </div>
                      <div class="col-sm-6" style="text-align: right">
                          <a class="btn btn-primary" href="{{ url('admin/assign_class_teacher/add') }}">Add New Assign Class
-                             Teacher</a>
+                             Teacher </a>
                      </div>
                  </div>
              </div><!-- /.container-fluid -->
          </section>
 
+         {{-- Search Assign Class Teacher Form --}}
+         <div class="row" style="margin:7.5px ">
+             <div class="col-md-12">
+                 <div class="card">
+                     <div class="card-header">
+                         <h3 class="card-title">Search Assign Class Teacher</h3>
+                     </div>
+                     <!-- form start -->
+                     <form action="" method="get">
+                         <div class="card-body">
+                             <div class="row">
+                                 <div class="form-group col-md-2">
+                                     <label>Class Name</label>
+                                     <input type="text" name="class_name" value="{{ Request::get('class_name') }}"
+                                         class="form-control" placeholder="Class name">
+                                 </div>
+                                 <div class="form-group col-md-2">
+                                     <label>Teacher Name</label>
+                                     <input type="text" name="teacher_name" value="{{ Request::get('teacher_name') }}"
+                                         class="form-control" placeholder="Teacher name">
+                                 </div>
+                                 <div class="form-group col-md-2">
+                                     <label>Status</label>
+                                     <select name="status" class="form-control">
+                                         <option value="">Select Status</option>
+                                         <option {{ Request::get('status') == 100 ? 'selected' : '' }} value="100">
+                                             Active
+                                         </option>
+                                         <option {{ Request::get('status') == 1 ? 'selected' : '' }} value="1">
+                                             Inactive</option>
+                                     </select>
+                                 </div>
+                                 <div class="form-group col-md-2" style="margin-bottom:0px;">
+                                     <label>Date</label>
+                                     <input type="date" name="date" value="{{ Request::get('date') }}"
+                                         class="form-control">
+                                 </div>
+                                 <div class="form-group col-md-3" style="margin-top:30px;">
+                                     <button type="submit" class="btn btn-primary">Search</button>
+                                     <a href="{{ url('admin/assign_class_teacher/list') }}"
+                                         class="btn btn-success">Reset</a>
+                                 </div>
+                             </div>
+                         </div>
+
+
+                     </form>
+                 </div>
+             </div>
+         </div>
 
          <!-- Main content -->
          <section class="content">
