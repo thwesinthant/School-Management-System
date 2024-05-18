9611275c494bb4116a006fc6687161dbe4a94a53
thwesinthant <thwesinthant4@gmail.com>
2024-05-18 14:54:33 +0630

    Part 53 completed

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\WeekModel;
 use Illuminate\Http\Request;
 use App\Models\ClassSubjectModel;
+use App\Models\ExamScheduleModel;
 use Illuminate\Support\Facades\Auth;
 use App\Models\ClassSubjectTimetableModel;
 
@@ -12,8 +13,17 @@ class CalendarController extends Controller
 {
     public function MyCalendar()
     {
+        $data['getTimetable'] = $this->getTimetable(Auth::user()->class_id);
+        $data['getExamTimetable'] = $this->getExamTimetable(Auth::user()->class_id);
+
+        $data['header_title'] = "My Calendar";
+        return view('student.my_calendar', $data);
+    }
+
+    public function getTimetable($class_id)
+    {
         $result = array();
-        $getRecord = ClassSubjectModel::MySubject(Auth::user()->class_id);
+        $getRecord = ClassSubjectModel::MySubject($class_id);
 
         foreach ($getRecord as $value) {
             $dataS['name'] = $value->subject_name;
@@ -37,8 +47,35 @@ class CalendarController extends Controller
             $dataS['week'] = $week;
             $result[] = $dataS;
         }
-        $data['getMyTimetable'] = $result;
-        $data['header_title'] = "My Calendar";
-        return view('student.my_calendar', $data);
+        return $result;
+    }
+
+    public function getExamTimetable($class_id)
+    {
+        $getExam = ExamScheduleModel::getExam($class_id);
+
+        $result = array();
+        // one than one row so loop it
+        foreach ($getExam as $value) {
+            $dataE = array();
+            $dataE['exam_name'] = $value->exam_name;
+            $getExamTimetable = ExamScheduleModel::getExamTimetable($value->exam_id, $class_id);
+            // loop that data array too , and bring value to show in blade file
+            $resultS = array();
+            foreach ($getExamTimetable as $valueS) {
+                $dataS = array();
+                $dataS['subject_name'] = $valueS->subject_name;
+                $dataS['exam_date'] = $valueS->exam_date;
+                $dataS['start_time'] = $valueS->start_time;
+                $dataS['end_time'] = $valueS->end_time;
+                $dataS['room_number'] = $valueS->room_number;
+                $dataS['full_marks'] = $valueS->full_marks;
+                $dataS['passing_mark'] = $valueS->passing_mark;
+                $resultS[] = $dataS;
+            }
+            $dataE['exam'] = $resultS;
+            $result[] = $dataE;
+        }
+        return $result;
     }
 }
--- resources/views/student/my_calendar.blade.php
+++ resources/views/student/my_calendar.blade.php
@@ -1,65 +1,85 @@
  @extends('layouts.app')
  @section('content')
-     <!-- Content Wrapper. Contains page content -->
-     <div class="content-wrapper">
-         <!-- Content Header (Page header) -->
-         <section class="content-header">
-             <div class="container-fluid">
-                 <div class="row mb-2">
-                     <div class="col-sm-6">
-                         <h1>My Calendar</h1>
-                     </div>
-                 </div>
-             </div>
-         </section>
-
-         <!-- Main content -->
-         <section class="content">
-             <div class="container-fluid">
-                 <div class="row">
-                     <div class="col-md-12">
-                         <div id="calendar">
-
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </section>
-     </div>
-     <!-- /.content-wrapper -->
+ @section('style')
+     <style type="text/css">
+         .fc-daygrid-event {
+             white-space: normal;
+         }
+     </style>
  @endsection
 
- @section('script')
-     <script src="{{ url('public/dist/fullcalendar/index.global.js') }}"></script>
-     <script type="text/javascript">
-         var events = new Array();
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+     <!-- Content Header (Page header) -->
+     <section class="content-header">
+         <div class="container-fluid">
+             <div class="row mb-2">
+                 <div class="col-sm-6">
+                     <h1>My Calendar</h1>
+                 </div>
+             </div>
+         </div>
+     </section>
 
-         @foreach ($getMyTimetable as $value)
-             @foreach ($value['week'] as $week)
-                 events.push({
-                     title: '{{ $value['name'] }}',
-                     daysOfWeek: '{{ $week['fullcalendar_day'] }}',
-                     startTime: '{{ $week['start_time'] }}',
-                     endTime: '{{ $week['end_time'] }}',
-                 });
-             @endforeach
+     <!-- Main content -->
+     <section class="content">
+         <div class="container-fluid">
+             <div class="row">
+                 <div class="col-md-12">
+                     <div id="calendar">
+
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
+ </div>
+ <!-- /.content-wrapper -->
+@endsection
+
+@section('script')
+ <script src="{{ url('public/dist/fullcalendar/index.global.js') }}"></script>
+ <script type="text/javascript">
+     var events = new Array();
+
+     @foreach ($getTimetable as $value)
+         @foreach ($value['week'] as $week)
+             events.push({
+                 title: '{{ $value['name'] }}',
+                 daysOfWeek: '{{ $week['fullcalendar_day'] }}',
+                 startTime: '{{ $week['start_time'] }}',
+                 endTime: '{{ $week['end_time'] }}',
+             });
          @endforeach
+     @endforeach
 
-         var calendarID = document.getElementById('calendar');
+     @foreach ($getExamTimetable as $valueE)
+         @foreach ($valueE['exam'] as $exam)
+             events.push({
+                 title: '{{ $valueE['exam_name'] }} - {{ $exam['subject_name'] }} ({{ date('h:i A', strtotime($exam['start_time'])) }} to {{ date('h:i A', strtotime($exam['end_time'])) }})',
+                 start: '{{ $exam['exam_date'] }}',
+                 end: '{{ $exam['exam_date'] }}',
+                 color: 'rgba(255, 0, 0, 0.84)',
+                 url: '{{ url('student/my_exam_timetable') }}',
+             });
+         @endforeach
+     @endforeach
 
-         var calendar = new FullCalendar.Calendar(calendarID, {
-             headerToolbar: {
-                 left: 'prev,next today',
-                 center: 'title',
-                 right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-             },
-             initialDate: '<?= date('Y-m-d') ?>',
-             navLinks: true, // can click day/week names to navigate views
-             editable: false,
-             events: events,
-             initialView: 'timeGridDay',
-         });
+     var calendarID = document.getElementById('calendar');
 
-         calendar.render();
-     </script>
- @endsection
+     var calendar = new FullCalendar.Calendar(calendarID, {
+         headerToolbar: {
+             left: 'prev,next today',
+             center: 'title',
+             right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+         },
+         initialDate: '<?= date('Y-m-d') ?>',
+         navLinks: true, // can click day/week names to navigate views
+         editable: false,
+         events: events,
+         //  initialView: 'timeGridDay',
+     });
+
+     calendar.render();
+ </script>
+@endsection
